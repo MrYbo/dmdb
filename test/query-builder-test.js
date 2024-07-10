491459740565addc1c1485121fad5684bd2b1dd0
mrybo <cn6bo.y@gmail.com>
2024-07-10 21:48:36 +0800

    feat: 对根据条件生成sql语句的方法进行更改

---
+++ test/query-builder-test.js
@@ -0,0 +1,18 @@
+
+const QueryBuilder = require('../lib/utils/query-builder');
+
+const criteria = {
+  select: ['a', 'b'],
+  where: {
+    a: 1,
+    and: [{ a: { '>': 2 } }, { b: 3 }],
+    or: [{ d: 1 }, { e: { '<=': 2 } }],
+    f: { '>': 3 },
+    createdAt: { '>': '2021-07-08 14:37:20' } // 时间字符串
+  },
+  limit: 10,
+  offset: 20,
+}
+const queryBuilder = new QueryBuilder('test', 'admin', criteria);
+const sql = queryBuilder.getQueryString();
+console.log(sql);
