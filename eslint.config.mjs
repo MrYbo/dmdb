240b41efebca90324e99760d467f1bcc349e20a2
mrybo <cn6bo.y@gmail.com>
2024-07-21 17:04:57 +0800

    fix: main not find

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  {ignores: ['**/build/**', '**/dist/**', 'node_modules/*']},
+  {ignores: ['**/build/**', 'node_modules/*']},
   {
     plugins: {
       '@typescript-eslint': tseslint.plugin,
